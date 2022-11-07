d93725ec1434d75c72539728cc150c3485fb9c0a
Ladibo <117651607+Ladibo@users.noreply.github.com>
2022-11-07 14:34:43 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,7 @@ class HelloWorld {
         System.out.println("Hello, World! <3"); 
         System.out.println("hejsan från david också!");
         // hej
+    //  hej hej
+        System.out.println("hejsan från david också!");
     }
 }
